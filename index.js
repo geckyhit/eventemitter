a4a95bed333267123dd57e11ad7f1a96c2c00d43
geckyhit <gekcy@mail.ru>
2018-05-01 13:14:52 +0300

    new file

---
+++ index.js
@@ -0,0 +1,42 @@
+class EventEmitter {
+    constructor() {
+        this.events = {}
+    }
+
+    on(evName, f) {
+        if (!this.events[evName])
+            this.events[evName] = [];
+
+        this.events[evName].push(f);
+    }
+
+    once(evName, f) {
+        const handler = (data) => {
+            this.off(evName, handler);
+            f(data);
+        }
+        this.on(evName, handler);
+
+        return () => this.off(evName, handler);
+    }
+
+    off(evName, f) {
+        const indexF = this.events[evName].indexOf(f);
+        if (indexF < 0) return;
+
+        this.events[evName] = [
+            ...this.events[evName].slice(0, indexF),
+            ...this.events[evName].slice(indexF + 1)
+        ];
+    }
+
+    emit(evName, data) {
+        this.events[evName].forEach(f => f(data));
+    }
+}
+
+
+const EE = new EventEmitter();
+const cancelOnce = EE.once('test', b);
+cancelOnce();
+EE.emit('test', '5');
