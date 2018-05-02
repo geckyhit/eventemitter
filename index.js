872d7bec86279fd64c61f1d7c87846cd99f914f0
geckyhit <gekcy@mail.ru>
2018-05-02 15:08:45 +0300

    update map

--- index.js
+++ index.js
@@ -15,57 +15,34 @@ class EventEmitter {
             this.off(evName, handler);
             f(data);
         }
-        this.on(evName, handler);
+        this.on(evName, handler)
 
         return handler;
     }
 
     off(evName, f) {
-        const indexF = this.events[evName].indexOf(f);
-        if (indexF < 0) return;
+        const indexF = this.events[evName].indexOf(f)
+        if (indexF < 0) return
 
         this.events[evName] = [
             ...this.events[evName].slice(0, indexF),
             ...this.events[evName].slice(indexF + 1)
-        ];
+        ]
     }
 
     emit(evName, data) {
         this.events[evName].forEach(f => f(data));
     }
 
-    map(modF) {
-
-        const parentEE = this;
-        const newEE = new EventEmitter();
-
-        const on = newEE.on;
-        newEE.on = (evName, f) => {
-
-            parentEE.on(evName, (data) =>
-                newEE.emit(evName, modF(data)
-                ))
-
-            on.call(newEE, evName, f);
+    map(mapFn) {
+        let emitter2 = new EventEmitter()
+        let _on = emitter2.on.bind(emitter2)
+        emitter2.on = (event, observeFn) => {
+            _on(event, observeFn)
+            this.on(event, (arg) => {
+                observeFn(mapFn(arg))
+            })
         }
-
-        const emit = newEE.emit;
-        newEE.emit = (evName, data) =>
-            emit.call(newEE, evName, modF(data))
-
-
-        return newEE;
+        return emitter2
     }
 }
-
-
-const e1 = new EventEmitter();
-const e2 = e1.map(x => {
-    return x * 2
-});
-
-e1.on('data', x => console.log('e1:', x));
-e2.on('data', x => console.log('e2:', x));
-
-e1.emit('data', 1);
-e1.emit('data', 2);
