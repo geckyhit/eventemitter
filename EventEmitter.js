318b2f1ab8e8e5bba6648512862c32bafba70447
geckyhit <gekcy@mail.ru>
2018-05-04 21:37:19 +0300

    test

--- EventEmitter.js
+++ EventEmitter.js
@@ -1,20 +1,25 @@
 class EventEmitter {
     constructor() {
-        this._id = Date.now() * Math.random();
+        this._id = EventEmitter.id++;
         this.events = {}
     }
 
     on(evName, f) {
         if (!this.events[evName])
-            this.events[evName] = [];
+            this.events[evName] = []
 
-        this.events[evName].push(f);
+
+        // if (typeof f === 'function')
+        //     f()
+
+        this.events[evName].push(f)
+        console.log('on', this._id, this.events)
     }
 
     once(evName, f) {
         const handler = (data) => {
-            this.off(evName, handler);
-            f(data);
+            this.off(evName, handler)
+            f(data)
         }
         this.on(evName, handler)
 
@@ -32,6 +37,7 @@ class EventEmitter {
     }
 
     emit(evName, data) {
+        if (!this.events[evName]) throw new Error('Non-existent event')
         this.events[evName].forEach(f => f(data));
     }
 
@@ -39,6 +45,7 @@ class EventEmitter {
         let emitter2 = new EventEmitter()
         let _on = emitter2.on.bind(emitter2)
         emitter2.on = (event, observeFn) => {
+            console.log('on in map', this._id);
             _on(event, observeFn)
             this.on(event, (arg) => {
                 observeFn(mapFn(arg))
@@ -47,5 +54,7 @@ class EventEmitter {
         return emitter2
     }
 }
+
+EventEmitter.id = 1;
 
 module.exports = EventEmitter
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const EventEmitter = require('./EventEmitter');
 const testEmitter = require('./testEmitter');
 
-let e1 = new EventEmitter()
+var e1 = new EventEmitter()
+let e2 = e1.map(x => x * 2)
+let e3 = e2.map(x => x * 2)
+let e4 = e3.map(x => x * 2)
+
+e4.on('data', (x) => console.log('e4:', x))
+
+e1.emit('data', 2)
